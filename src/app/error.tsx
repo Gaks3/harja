5bf4068238d997915353d7a42d0e09d6d187e7cd
Gaks3 <dwi66116@gmail.com>
2024-09-16 13:38:51 +0700

    feat: error page

---
+++ src/app/error.tsx
@@ -0,0 +1,31 @@
+"use client"; // Error components must be Client Components
+
+import { useEffect } from "react";
+import { Button } from "./_components/ui/button";
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  useEffect(() => {
+    // Log the error to an error reporting service
+    console.error(error);
+  }, [error]);
+
+  return (
+    <div className="flex h-screen w-full flex-col items-center justify-center gap-5">
+      <h1 className="text-2xl font-bold">Something went wrong!</h1>
+      <Button
+        onClick={
+          // Attempt to recover by trying to re-render the segment
+          () => reset()
+        }
+      >
+        Try again
+      </Button>
+    </div>
+  );
+}
