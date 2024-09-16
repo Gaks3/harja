c76e288f30bc86c70e20567a57d5cf03fc7b3a08
Gaks3 <dwi66116@gmail.com>
2024-09-16 13:37:15 +0700

    feat: webpack, image, and redirect permanent config

--- next.config.js
+++ next.config.js
@@ -5,6 +5,33 @@
 await import("./src/env.js");
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+  webpack: (config) => {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
+    config.externals.push("@node-rs/argon2", "@node-rs/bcrypt");
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+    return config;
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        hostname: "localhost",
+        port: "3000",
+      },
+    ],
+  },
+  async redirects() {
+    return [
+      {
+        source: "/dashboard",
+        destination: "/dashboard/tools",
+        permanent: true,
+      },
+    ];
+  },
+};
 
 export default config;
