74ac3a45f7eb9ef9e3b45d68ec74dbff1d1f47e4
Gaks3 <dwi66116@gmail.com>
2024-09-18 21:39:28 +0700

    feat: changing the content

--- src/app/_components/home/hero.tsx
+++ src/app/_components/home/hero.tsx
@@ -19,36 +19,35 @@ export default function Hero() {
         <div className="mx-auto flex min-h-screen max-w-3xl flex-col items-center justify-center">
           <div className="hidden sm:mb-8 sm:flex sm:justify-center">
             <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
-              Announcing the projects we have created.{" "}
+              Solusi Baru untuk Kebutuhan Anda.{" "}
               <Link href="#" className="font-semibold text-[#188753]">
                 <span aria-hidden="true" className="absolute inset-0" />
-                Read more <span aria-hidden="true">&rarr;</span>
+                Pelajari Lebih lanjut <span aria-hidden="true">&rarr;</span>
               </Link>
             </div>
           </div>
           <div className="text-center">
-            <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+            <h1 className="text-5xl font-bold tracking-tight text-gray-900 sm:text-6xl">
               Harja Smart Greenhouse
             </h1>
-            <p className="mt-6 text-lg leading-8 text-gray-600">
+            <p className="mt-6 text-base leading-8 text-gray-600 md:text-lg">
               Harja Smart Greenhouse menawarkan solusi greenhouse dengan
-              teknologi otomatis. Sistem kami memantau kondisi tanaman secara
-              real-time untuk meningkatkan efisiensi. Temukan cara cerdas
-              menanam dan menghemat sumber daya dengan teknologi kami.
+              teknologi terbarukan. Solusi inovatif untuk menghadapi pertanian
+              modern dan ketahanan pangan.
             </p>
             <div className="mt-7 flex items-center justify-center gap-x-6">
               <Link
                 href="/login"
                 className="rounded-md bg-[#188753] px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-[#145d40] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[#145d40]"
               >
-                Get started
+                Daftar Sekarang
               </Link>
-              <a
-                href="#"
+              <Link
+                href="#visi-misi"
                 className="text-sm font-semibold leading-6 text-gray-900"
               >
-                Learn more <span aria-hidden="true">→</span>
-              </a>
+                Selengkapnya <span aria-hidden="true">→</span>
+              </Link>
             </div>
           </div>
         </div>
