5dab7068faec0bf97492040820099a16ece3cd70
Gaks3 <dwi66116@gmail.com>
2024-09-18 21:40:44 +0700

    initial commit workflow component

---
+++ src/app/_components/home/workflow.tsx
@@ -0,0 +1,8 @@
+export default function Workflow() {
+  return (
+    <section className="mx-10 mt-20 space-y-10 lg:mx-auto">
+      <h2 className="text-center text-4xl font-bold">Cara Kerja Kami</h2>
+      <div className="grid grid-cols-2 gap-x-10 gap-y-5"></div>
+    </section>
+  );
+}
